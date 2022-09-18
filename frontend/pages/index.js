775d0f1f81bd94f9b12d9dc12406188b1949a5d9
reallywhoami <reallytwysocki@gmail.com>
2022-09-19 01:05:39 +0200

    Change connecting to ethereum

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,9 @@
 import { ethers } from 'ethers'
-import { useEffect, useLayoutEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Input from '../components/Input'
 import Donate from '../components/Donate'
 
 import { contractAddress } from '../config'
-const { ethereum } = window
 
 import BuyMeATea from './artifacts/contracts/BuyMeATea.sol/BuyMeATea.json'
 
@@ -13,19 +12,16 @@ export default function Home() {
   const [name, setName] = useState('')
   const [message, setMessage] = useState('')
   const [value, setValue] = useState()
-  const [currentAccount, setCurrentAccount] = useState('')
+  const [provider, setProvider] = useState()
 
   const connectWallet = async () => {
     try {
-      if (!ethereum) {
+      if (!window.ethereum) {
         console.log('please install MetaMask')
       }
 
-      const accounts = await ethereum.request({
-        method: 'eth_requestAccounts',
-      })
-
-      setCurrentAccount(accounts[0])
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      setProvider(provider)
     } catch (error) {
       console.log(error)
     }
@@ -33,36 +29,39 @@ export default function Home() {
 
   const buyTea = async () => {
     try {
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum, 'any')
-        const signer = provider.getSigner()
-        const buyMeATea = new ethers.Contract(
-          contractAddress,
-          BuyMeATea.abi,
-          signer
-        )
-
-        console.log('buying tea..')
-        const teaTxn = await buyMeATea.donate(
-          name ? name : 'Anonymous',
-          message ? message : 'Enjoy your tea!',
-          value
-            ? { value: ethers.utils.parseEther(value.toString()) }
-            : { value: ethers.utils.parseEther('0.001') }
-        )
-
-        await teaTxn.wait()
-
-        console.log('mined ', teaTxn.hash)
-
-        console.log('tea purchased!')
-        getDonates()
-
-        // Clear the form fields.
-        setName('')
-        setMessage('')
-        setValue()
+      if (!provider) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        setProvider(provider)
       }
+      await provider.send('eth_requestAccounts', []) // <- this promps user to connect metamask
+      const signer = provider.getSigner()
+
+      const buyMeATea = new ethers.Contract(
+        contractAddress,
+        BuyMeATea.abi,
+        signer
+      )
+
+      console.log('buying tea..')
+      const teaTxn = await buyMeATea.donate(
+        name ? name : 'Anonymous',
+        message ? message : 'Enjoy your tea!',
+        value
+          ? { value: ethers.utils.parseEther(value.toString()) }
+          : { value: ethers.utils.parseEther('0.001') }
+      )
+
+      await teaTxn.wait()
+
+      console.log('mined ', teaTxn.hash)
+
+      console.log('tea purchased!')
+      getDonates()
+
+      // Clear the form fields.
+      setName('')
+      setMessage('')
+      setValue()
     } catch (error) {
       console.log(error)
     }
@@ -83,13 +82,13 @@ export default function Home() {
   // Function to fetch all donates stored on-chain.
   const getDonates = async () => {
     try {
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
+      if (provider) {
+        // const provider = new ethers.providers.Web3Provider(window.ethereum)
+        // const signer = provider.getSigner()
         const buyMeATea = new ethers.Contract(
           contractAddress,
           BuyMeATea.abi,
-          signer
+          provider
         )
 
         // console.log('fetching donates from the blockchain..')
@@ -106,8 +105,11 @@ export default function Home() {
 
   useEffect(() => {
     connectWallet()
-    getDonates()
   }, [])
+
+  useEffect(() => {
+    getDonates()
+  }, [provider])
 
   return (
     <div className='main flex justify-center mt-10'>
@@ -117,10 +119,8 @@ export default function Home() {
             If you like my work, please support me!
             <p className='mt-7'>☕</p>
           </h1>
-          {!currentAccount && (
-            <h2 className='text-3xl text-center'>Connect to your wallet</h2>
-          )}
-          {currentAccount && (
+
+          {provider && (
             <>
               <h2 className='text-3xl text-center mb-4'>Buy me a tea!</h2>
               <div className='flex flex-col gap-5 items-center'>
@@ -141,12 +141,12 @@ export default function Home() {
             </>
           )}
         </div>
-        {currentAccount && (
+        {provider && (
           <h1 className='mt-8 mb-4 text-2xl text-gray-800 font-medium font-[Roboto] px-5'>
             Recent donates
           </h1>
         )}
-        {currentAccount && (
+        {provider && (
           <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-8 gap-x-4 mb-8 px-5'>
             {donates
               .slice(0)
